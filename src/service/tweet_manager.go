6e55f4ffeef54524fe52d42963bc11a2b8bc997d
Pato <patoacion@gmail.com>
2019-08-20 16:41:41 -0300

    Refactor test

--- src/service/tweet_manager.go
+++ src/service/tweet_manager.go
@@ -1,11 +1,13 @@
 package service
 
-var Tweet string
+import "github.com/Patosp96/twitterAccelerator/src/domain"
 
-func PublishTweet(tweet string) {
+var Tweet *domain.Tweet
+
+func PublishTweet(tweet *domain.Tweet) {
 	Tweet = tweet
 }
 
-func GetTweet() string {
+func GetTweet() *domain.Tweet {
 	return Tweet
 }
--- src/service/tweet_manager_test.go
+++ src/service/tweet_manager_test.go
@@ -1,18 +1,30 @@
 package service_test
 
 import (
+	"github.com/Patosp96/twitterAccelerator/src/domain"
 	"github.com/Patosp96/twitterAccelerator/src/service"
 	"testing"
 )
 
 func TestPublishedTweetIsSaved(t *testing.T) {
 
-	var tweet string = "This is my first tweet"
+	// Initialization
+	var tweet *domain.Tweet
+	user := "grupoesfera"
+	text := "This is my first tweet"
+	tweet = domain.NewTweet(user, text)
 
+	// Operation
 	service.PublishTweet(tweet)
 
-	if service.GetTweet() != tweet {
-		t.Error("Expected tweet is", tweet)
+	// Validation
+	publishedTweet := service.GetTweet()
+	if publishedTweet.User != user &&
+		publishedTweet.Text != text {
+		t.Errorf("Expected tweet is %s: %s \nbut is %s: %s",
+			user, text, publishedTweet.User, publishedTweet.Text)
 	}
-
+	if publishedTweet.Date == nil {
+		t.Error("Expected date can't be nil")
+	}
 }
