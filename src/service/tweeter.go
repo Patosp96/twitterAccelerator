310c1665f73337cb2960637d2f8fd9f650b3ab39
Pato <patoacion@gmail.com>
2019-08-20 15:18:23 -0300

    Se modifica el test y se agrega nuevo getter para el tweet

---
+++ src/service/tweeter.go
@@ -0,0 +1,49 @@
+package service
+
+import (
+	"github.com/abiosoft/ishell"
+)
+
+func main() {
+
+	shell := ishell.New()
+	shell.SetPrompt("Tweeter >> ")
+	shell.Print("Type 'help' to know commands\n")
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "publishTweet",
+		Help: "Publishes a tweet",
+		Func: func(c *ishell.Context) {
+
+			defer c.ShowPrompt(true)
+
+			c.Print("Write your tweet: ")
+
+			tweet := c.ReadLine()
+
+			PublishTweet(tweet)
+
+			c.Print("Tweet sent\n")
+
+			return
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "showTweet",
+		Help: "Shows a tweet",
+		Func: func(c *ishell.Context) {
+
+			defer c.ShowPrompt(true)
+
+			tweet := GetTweet()
+
+			c.Println(tweet)
+
+			return
+		},
+	})
+
+	shell.Run()
+
+}
